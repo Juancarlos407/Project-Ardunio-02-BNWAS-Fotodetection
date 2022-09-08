215873db3a5070dfacc5d8128bc5b2f482b84794
JuanCarlos407 <31823343+Juancarlos407@users.noreply.github.com>
2022-09-08 18:57:07 +0200

    Add files via upload

---
+++ Fotodetectionbnwas.ino
@@ -0,0 +1,29 @@
+
+int A=9; //Led//
+int foto=3; //Fotoresistencia en el pin analogo A3//
+int measure=0;
+int light=0;
+
+void setup()
+{
+ {
+  pinMode(A,OUTPUT); //declaramos los leds como salidas//
+ }
+}
+
+void loop() 
+{
+ measure=analogRead (foto); //lectura de la fotoresistencia//
+ Serial.println("Lectura ");
+ Serial.println(measure);
+ delay (500);
+    if (measure < 1)
+{ digitalWrite(A,HIGH);
+delay (200);
+digitalWrite(A,LOW);
+delay (50);}
+ 
+    if (measure > 1) 
+  {digitalWrite(A,LOW);}
+ delay (500);
+}
